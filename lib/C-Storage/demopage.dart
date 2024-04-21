669a06532c996577ed044337517f4026bc8cbaa7
Krishna Gupta <krishna.ghhh@gmail.com>
2024-04-21 21:09:06 +0530

    Image Picker Added

--- lib/C-Storage/demopage.dart
+++ lib/C-Storage/demopage.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutterbase/Components/ui_components.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DemoPage extends StatefulWidget {
   const DemoPage({super.key});
@@ -11,31 +15,54 @@ class DemoPage extends StatefulWidget {
 class _DemoPageState extends State<DemoPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  File? pickedImage;
 
-  showAlertBox() {
-    return showDialog(
+  // Function to display the image selection dialog
+  void showAlertBox() {
+    showDialog(
       context: context,
       builder: (BuildContext context) {
-        return const AlertDialog(
-          title: Text(
-            'Pick Image From',
-          ),
+        return AlertDialog(
+          title: const Text('Pick Image From'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               ListTile(
-                leading: Icon(Icons.camera_alt),
-                title: Text('Camera'),
+                onTap: () {
+                  pickImage(ImageSource.camera);
+                  Navigator.pop(context);
+                },
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Camera'),
               ),
               ListTile(
-                leading: Icon(Icons.image),
-                title: Text('Gallery'),
+                onTap: () {
+                  pickImage(ImageSource.gallery);
+                  Navigator.pop(context);
+                },
+                leading: const Icon(Icons.image),
+                title: const Text('Gallery'),
               ),
             ],
           ),
         );
       },
     );
+  }
+
+  // Function to handle image picking
+  Future<void> pickImage(ImageSource source) async {
+    try {
+      final picker = ImagePicker();
+      final pickedFile = await picker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          pickedImage = File(pickedFile.path);
+        });
+      }
+    } catch (e) {
+      log(e.toString());
+    }
   }
 
   @override
@@ -52,36 +79,35 @@ class _DemoPageState extends State<DemoPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               InkWell(
-                onTap: () {
-                  showAlertBox();
-                },
-                child: const CircleAvatar(
+                onTap: showAlertBox,
+                child: CircleAvatar(
                   radius: 80,
-                  child: Icon(Icons.add_a_photo_rounded, size: 80),
+                  backgroundImage:
+                      pickedImage != null ? FileImage(pickedImage!) : null,
+                  child: pickedImage == null
+                      ? const Icon(Icons.add_a_photo_rounded, size: 80)
+                      : null,
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               Components.customTextField(
                 emailController,
                 "Email",
                 Icons.mail,
                 false,
               ),
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 10),
               Components.customTextField(
                 passwordController,
                 "Password",
                 Icons.password,
                 true,
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Components.customButton(() {}, 'Sign Up')
+              const SizedBox(height: 20),
+              Components.customButton(
+                () {},
+                'Sign Up',
+              )
             ],
           ),
         ),
